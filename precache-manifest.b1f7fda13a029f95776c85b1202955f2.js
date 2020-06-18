f5cf8832988b710ba9daeed93ed29ab6d7841bcf
ShapovalDenys <denys.igorevich@gmail.com>
2020-06-18 18:51:08 +0300

    Updates

---
+++ precache-manifest.b1f7fda13a029f95776c85b1202955f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9b8f4fc8311b178e77b683e794ffc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5664ef25047e4f0d02c",
+    "url": "./static/css/main.d2d05995.chunk.css"
+  },
+  {
+    "revision": "77f52ae1d3c7f2037384",
+    "url": "./static/js/2.2ea1b0bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2ea1b0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5664ef25047e4f0d02c",
+    "url": "./static/js/main.611ce14f.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "./static/media/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "./static/media/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "93f4f9f6e3064491ee683f5a1b2216b6",
+    "url": "./static/media/angle-bg-min.93f4f9f6.png"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "af52f2f8a51fb612a251621587a592a5",
+    "url": "./static/media/parallax-1-min.af52f2f8.jpg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "./static/media/themify.e23a7dca.ttf"
+  }
+]);
